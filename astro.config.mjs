37c01f958deccca84ad3c75d3dbb20cb121d8078
Karl Lieser <karl@triffecta.com>
2025-03-14 20:33:59 +0000

    Include particles for pages
    Include particles setup and publish for use
    Minify json and js files in the dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,47 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import { minify } from "terser";
 
 // https://astro.build/config
 export default defineConfig({
-  // Setup for base path before domain moved to production
+  /*
+    ##########################################
+    Setup for base path before domain moved to production
+    ##########################################
+  */
   output: "static",
   base: "/marketing-site",
   site: "http://localhost:4321/marketing-site/",
-  //
+  /*
+    ##########################################
+    ##########################################
+  */
   integrations: [preact()],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: "public/js/vendor/*.js",
+            dest: "js/vendor",
+            transform: async (content) => {
+              const minified = await minify(content.toString());
+              return minified.code || null;
+            },
+          },
+          {
+            src: "public/js/*.json",
+            dest: "js",
+            transform: async (content) => {
+              const json = JSON.parse(content.toString());
+              return JSON.stringify(json) || null;
+            },
+          },
+        ],
+      }),
+    ],
   },
 });
