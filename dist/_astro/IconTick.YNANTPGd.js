621cd2a51153851a6b1f30b7596840b0814db120
Karl Lieser <karl@triffecta.com>
2025-04-10 19:05:22 +0100

    Publish site with contact us

---
+++ dist/_astro/IconTick.YNANTPGd.js
@@ -0,0 +1,1 @@
+import{u as n}from"./jsxRuntime.module.WrhyN42J.js";function e({size:o="40px",strokeColor:t="#FFFFFF"}){return n("svg",{width:o,height:o,stroke:t,viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:n("path",{d:"M5.5 12.5L10.167 17L19.5 8",stroke:t,"stroke-width":"2","stroke-linecap":"round","stroke-linejoin":"round"})})}export{e as I};
